03e66467715159055650a8f2dd757d4d59a0cba2
imranhei <imranhossainrana.2014@gmail.com>
2023-12-01 12:22:00 +0600

    indutry page make responsive and update card

--- src/components/Industry/Industry.jsx
+++ src/components/Industry/Industry.jsx
@@ -3,19 +3,55 @@ import { getIndustry } from "../../data/industry";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Partner from "../Shared/PartnerWithUs";
+import Slider from "react-slick";
 
 const Industry = () => {
-  const [data, setData] = useState([]); // [data, setData
+  const [data, setData] = useState([]);
   const params = useParams();
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    speed: 500,
+    slidesPerRow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    initialSlide: 0,
+    gap: 20,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
+  };
+
   useEffect(() => {
-    window.scrollTo(0, 0);
+    // window.scrollTo(0, 0);
     const temp = getIndustry(params.industryId);
     setData(temp);
-    console.log(temp);
   }, [params.industryId]);
 
-  if(data === undefined || data === null) return (<div className="container mx-auto relative z-10 lg:pt-44 sm:pt-36 pt-24 flex flex-col items-center">No data found</div>)
+  if (data === undefined || data === null)
+    return (
+      <div className="container mx-auto relative z-10 lg:pt-44 sm:pt-36 pt-24 flex flex-col items-center">
+        No data found
+      </div>
+    );
 
   if (data.length === 0) return <div>Loading...</div>;
 
@@ -25,26 +61,30 @@ const Industry = () => {
         <p className="lg:text-5xl sm:text-[40px] text-2xl font-bold tracking-wide font-exo lg:pb-16 sm:pb-14 pb-8 px-10 text-amBlue text-center">
           {data.title || ""}
         </p>
-        <div className="text-center text-amBlue lg:text-lg sm:text-base text-sm font-semibold lg:leading-10 leading-6 tracking-wide font-khula sm:pb-14 pb-8 sm:px-20 px-10">
-            {data.description || ""}
+        <div className="md:text-center text-justify text-amBlue lg:text-lg sm:text-base text-sm font-semibold lg:leading-10 leading-6 tracking-wide font-khula sm:pb-14 pb-8 lg:px-20 px-10">
+          {data.description || ""}
         </div>
-        <img src={data.banner} alt="background" className="w-full pb-10 px-20" />
-        {/* <div className="container mx-auto px-10">
+        <img
+          src={data.banner}
+          alt="background"
+          className="w-full pb-10 lg:px-20 px-10"
+        />
+        <div className="container mx-auto lg:px-20 px-10">
           <Slider
             {...settings}
             className="card mx-auto pb-10 overflow-hidden z-10"
           >
-            {data.map((item, index) => (
+            {data?.card && data.card.map((item, index) => (
               <div
                 key={index}
-                className="w-full h-full flex justify-center p-4"
+                className="w-full h-full flex justify-center sm:p-4 p-2"
               >
                 <div className="flex justify-center">
-                  <div className="bg-amCard hover:ring-1 ring-amYellow/30 hover:bg-gray-100 rounded-xl p-8 sm:w-[370px] w-80 sm:h-[450px] h-[380px] shadow-md space-y-2 relative z-10">
-                    <div className="sm:h-36 h-24 sm:w-36 w-24 sm:scale-100 scale-90 mx-auto flex justify-center items-center">
+                  <div className="bg-amCard hover:bg-gray-100 rounded-xl p-8 sm:w-[350px] w-80 md:h-[470px] sm:h-[410px] h-[350px] shadow-md space-y-2 relative z-10">
+                    <div className="sm:h-36 h-16 sm:w-36 w-24 sm:scale-100 scale-90 mx-auto flex justify-center items-center">
                       {item.icon}
                     </div>
-                    <p className="font-bold font-exo text-amBlue pt-4 tracking-widest sm:text-xl text-center pb-2">
+                    <p className="font-bold font-exo text-amBlue pt-4 tracking-widest lg:text-xl sm:text-lg text-center pb-2">
                       {item.title}
                     </p>
                     <p className="font-khula text-amBlue md:text-base text-sm sm:text-center text-justify lg:leading-7">
@@ -55,15 +95,17 @@ const Industry = () => {
               </div>
             ))}
           </Slider>
-        </div> */}
-        <div className="flex lg:gap-20 gap-10 sm:px-20 px-10 py-10">
-          {data.imgs && data.imgs.length > 0 ? <img src={data.imgs[0]} alt="" className="flex-1" /> : null} 
+        </div>
+        <div className="lg:px-20 px-10 py-10">
+          <div className="xl:w-fit w-1/2 float-none mx-auto md:float-left pb-10 md:pb-0">
+          {data.imgs && data.imgs.length > 0 ? <img src={data.imgs[0]} alt="" className="xl:pr-20 md:pr-10 md:pb-5" /> : null}
+          </div>
           <div className="text-amBlue flex-1">
-            <p className="font-exo font-bold lg:text-4xl sm:text-3xl text-xl pb-10">
-              {data.subTitle || ''}
+            <p className="font-exo font-bold lg:text-4xl sm:text-3xl text-xl sm:pb-10 pb-5">
+              {data.subTitle || ""}
             </p>
             <p className="font-khula leading-7 pb-10 text-justify whitespace-pre-line">
-                {data.subDescription || ''}
+              {data.subDescription || ""}
             </p>
             <Link
               to="/contact"
@@ -87,42 +129,50 @@ const Industry = () => {
         </div>
       </div>
       <div className="w-full py-20 container mx-auto lg:px-20 px-10 text-amBlue text-justify">
-      <p className="lg:text-4xl sm:text-3xl text-2xl font-exo font-bold pb-20">
-        {data.subHeader || ''}
-      </p>
-      <p className="lg:text-2xl sm:text-xl text-lg font-exo font-bold pb-4">
-        {data.point_01 || ''}
-      </p>
-      <p className="font-khula sm:text-base text-sm pb-6">
-        {data.point_01_description || ''}
-      </p>
+        <p className="lg:text-4xl sm:text-2xl text-xl font-exo font-bold md:pb-20 pb-10">
+          {data.subHeader || ""}
+        </p>
+        <p className="lg:text-2xl sm:text-xl text-lg font-exo font-bold pb-4 text-left">
+          {data.point_01 || ""}
+        </p>
+        <p className="text-justify text-amBlue sm:text-base text-sm lg:leading-10 leading-6 tracking-wide font-khula sm:pb-10 pb-6">
+          {data.point_01_description || ""}
+        </p>
 
-      <p className="lg:text-2xl sm:text-xl text-lg font-exo font-bold pb-4">
-        {data.point_02 || ''}
-      </p>
-      <p className="font-khula sm:text-base text-sm pb-6">
-        {data.point_02_description || ''}
-      </p>
+        <p className="lg:text-2xl sm:text-xl text-lg font-exo font-bold pb-4">
+          {data.point_02 || ""}
+        </p>
+        <p className="text-justify text-amBlue sm:text-base text-sm lg:leading-10 leading-6 tracking-wide font-khula sm:pb-10 pb-6">
+          {data.point_02_description || ""}
+        </p>
 
-      <p className="lg:text-2xl sm:text-xl text-lg font-exo font-bold pb-4">
-        {data.point_03 || ''}
-      </p>
-      <p className="font-khula sm:text-base text-sm pb-6">
-        {data.point_03_description || ''}
-      </p>
+        <p className="lg:text-2xl sm:text-xl text-lg font-exo font-bold pb-4">
+          {data.point_03 || ""}
+        </p>
+        <p className="text-justify text-amBlue sm:text-base text-sm lg:leading-10 leading-6 tracking-wide font-khula sm:pb-10 pb-6">
+          {data.point_03_description || ""}
+        </p>
 
-      <p className="lg:text-2xl sm:text-xl text-lg font-exo font-bold pb-4">
-        {data.point_04 || ''}
-      </p>
-      <p className="font-khula sm:text-base text-sm">
-        {data.point_04_description || ''}
-      </p>
-      <div className="flex justify-between gap-10 py-20">
-       {data.imgs && data.imgs.length > 1 ? <img src={data.imgs[1]} alt="" className="w-3/5 object-cover" /> : <></>}
-       {data.imgs && data.imgs.length > 2 ? <img src={data.imgs[2]} alt="" className="flex-1 object-cover"/> : <></>}
+        <p className="lg:text-2xl sm:text-xl text-lg font-exo font-bold pb-4">
+          {data.point_04 || ""}
+        </p>
+        <p className="text-justify text-amBlue sm:text-base text-sm lg:leading-10 leading-6 tracking-wide font-khula sm:pb-6 pb-2">
+          {data.point_04_description || ""}
+        </p>
+        <div className="flex justify-between sm:py-20 py-10 w-full">
+          {data.imgs && data.imgs.length > 1 ? (
+            <img src={data.imgs[1]} alt="" className="w-3/5 object-cover " />
+          ) : (
+            <></>
+          )}
+          {data.imgs && data.imgs.length > 2 ? (
+            <img src={data.imgs[2]} alt="" className="w-1/3 object-cover" />
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
-    </div>
-    <Partner />
+      <Partner />
     </div>
   );
 };
